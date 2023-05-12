a920b73efe8772cfde1c7523abc5f00dbf3b57a4
JennyPersichini <nenny1096@gmail.com>
2023-05-12 16:27:44 +0200

    migrations

---
+++ csharp-ef-players/Migrations/20230512142653_FirstMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace csharp_ef_players.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "playerList",
+                columns: table => new
+                {
+                    PlayerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cognome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Punteggio = table.Column<double>(type: "float", nullable: false),
+                    PartiteGiocate = table.Column<int>(type: "int", nullable: false),
+                    PartiteVinte = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_playerList", x => x.PlayerId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_playerList_PlayerId",
+                table: "playerList",
+                column: "PlayerId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "playerList");
+        }
+    }
+}
