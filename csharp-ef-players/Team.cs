ee6ca370d5fd0a71af257ffa8494a8b2f52d358f
JennyPersichini <nenny1096@gmail.com>
2023-05-12 16:42:42 +0200

    add team

---
+++ csharp-ef-players/Team.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ef_players
+{
+    [Table("team")]
+    public class Team
+    {
+        //Una squadra è caratterizzata da: id, nome, città, allenatore, colori
+        public int TeamId { get; set; }
+        public string Nome { get; set; }
+        public string Citta { get; set; }
+        public string Allenatore { get; set; }
+        public string Colori { get; set; }
+
+        public Team(string nome, string citta, string allenatore, string colori)
+        {
+            Nome = nome;
+            Citta = citta;
+            Allenatore = allenatore;
+            Colori = colori;
+        }
+
+        public override string ToString()
+        {
+            string rapStringa = "Nome della squadra: " 
+                + Nome 
+                + "\n Città: " 
+                + Citta 
+                + "\n Allenatore: "
+                + Allenatore
+                + "\n Colori: "
+                + Colori;
+
+            return rapStringa;
+        }
+
+    }
+}
